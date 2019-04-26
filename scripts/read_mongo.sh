b3a9950b9408959ac820e54a55b5b9de216f7a35
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-04-26 19:50:48 +0200

    add printjson

--- scripts/read_mongo.sh
+++ scripts/read_mongo.sh
@@ -2,6 +2,6 @@
 set -ex
 echo "fooooooooooo"
 mongo mongo:27017/mydb <<EOF
-db.mycollection.find({transaction_price: { \$gt: 2000}});
-db.mycollection.find({}).count();
+db.mycollection.find({transaction_price: { \$gt: 2000}}).forEach( printjson );
+db.mycollection.find({}).count().forEach( printjson );
 EOF
