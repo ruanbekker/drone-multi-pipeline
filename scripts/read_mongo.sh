7b585f5910a3c01a1ed95c1c9082014ddb0b0445
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-04-28 22:08:01 +0200

    find only more than 900

--- scripts/read_mongo.sh
+++ scripts/read_mongo.sh
@@ -3,5 +3,5 @@ set -ex
 echo "fooooooooooo"
 mongo mongo:27017/mydb <<EOF
 db.mycollection.find().count();
-db.mycollection.find({transaction_price: { \$gt: 900}}).forEach( printjson );
+db.mycollection.find({transaction_price: { \$gt: 970}}).forEach( printjson );
 EOF
