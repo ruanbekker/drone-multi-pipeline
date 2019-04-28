4aa6718d09d925e9f5f733a907ae341f393b1c64
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-04-28 22:04:37 +0200

    test find change

--- scripts/read_mongo.sh
+++ scripts/read_mongo.sh
@@ -3,5 +3,5 @@ set -ex
 echo "fooooooooooo"
 mongo mongo:27017/mydb <<EOF
 db.mycollection.find().count();
-db.mycollection.find({transaction_price: { \$gt: 1}}).forEach( printjson );
+db.mycollection.find({transaction_price: { \$gt: 1000}}).forEach( printjson );
 EOF
